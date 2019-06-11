bfcf9cafc6cb13beb4697c1a140999191dfe58e3
HuyChocco <14520385@gm.uit.edu.vn>
2019-06-11 16:20:13 +0700

    them giỏ hàng, thanh toán, đăng ký, đăng nhập

---
+++ WebSitebanSach/WebSitebanSach/Models/GioHang.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSiteBanSach.Models
+{
+	[Serializable]
+	public class GioHang
+	{
+
+
+		public Sach Sach { set; get; }
+		public int SoLuong { set; get; }
+
+
+	}
+}
